75917f63b932b1bdcadc8b63a88271ff2dda926a
Valery Melou <me@valerymelou.com>
2022-04-19 16:51:30 +0100

    feat: search places with nomination

--- src/app/places/place.service.ts
+++ src/app/places/place.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Place } from './place';
@@ -7,18 +7,21 @@ import { Place } from './place';
   providedIn: 'root'
 })
 export class PlaceService {
+  private url = 'https://nominatim.openstreetmap.org/';
 
   constructor(private http: HttpClient) {}
 
   getPlaces(name: string): Observable<Place[]> {
-    return this.http.get<Place[]>('assets/data/places.json')
+    let params = new HttpParams();
+    params = params.set('q', name);
+    params = params.set('format', 'json');
+    params = params.set('limit', 10);
+    return this.http.get<Place[]>(this.url, { params: params })
       .pipe(
         map((data: any) => {
           const results: Place[] = [];
           data.forEach((item: any) => {
-            if (item.name.toLowerCase().indexOf(name.toLowerCase()) > -1) {
-              results.push(new Place(item.name, item.type));
-            }
+            results.push(new Place(item.display_name, item.type));
           });
 
           return results;
