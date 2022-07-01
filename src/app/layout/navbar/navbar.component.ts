a016902fa451fc9f640654cef95e9c96f2a7fdf5
Valery Melou <me@valerymelou.com>
2022-07-01 01:32:06 +0100

    fix: display of navabr on small devices

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Event, NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import { Coordinate } from 'ol/coordinate';
-import { fromLonLat } from 'ol/proj';
 import { AuthService } from 'src/app/auth/auth.service';
 import { MapService } from 'src/app/map/map.service';
 import { OscFormComponent } from 'src/app/oscs/osc-form/osc-form.component';
@@ -26,7 +24,7 @@ export class NavbarComponent implements OnInit {
     },
     {
       link: '/a-propos',
-      label: 'À propos',
+      label: 'À-propos',
       image: '/assets/icons/menu/about.svg',
       activeImage: '/assets/icons/menu/about-active.svg',
       active: false
