7832fd04c6d253ac1c7f27389130f6922c077c64
Valery Melou <valery.melou@mboadigital.tech>
2023-10-03 01:52:19 +0100

    refactor: upgrade angular from v15 to v16

--- src/app/core/i18n/i18n.resolver.ts
+++ src/app/core/i18n/i18n.resolver.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  Router,
-  Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
-} from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { I18nService } from './i18n.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class I18nResolver implements Resolve<boolean> {
+export class I18nResolver {
   constructor(private i18n: I18nService) {}
 
   resolve(
